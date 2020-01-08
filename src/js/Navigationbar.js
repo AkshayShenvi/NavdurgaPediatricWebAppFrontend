185a497e1e44b9bbc478f9f8c82ccc405e7eb558
Yugesha Sapte <yugeshasapte.4@gmail.com>
2020-01-08 00:59:57 -0600

    Added calendar date change event

--- src/js/Navigationbar.js
+++ src/js/Navigationbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Logo from '../img/logo.png';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Router, Switch, Route, Link } from "react-router-dom";
 import Settings from './Settings'
 import LandingPage from './LandingPage';
 import Button from 'react-bootstrap/Button'
@@ -15,7 +15,7 @@ const Navigationbar = ({onRouteChange}) =>{
                     <title>Navdurga Fracture And Accident Center</title>
                     <nav className="navbar navbar-expand-lg navbar-light bg-light">
                         <ul className="navbar-nav mr-auto">
-                            <li><Link to={'#'} className="navbar-brand"> 
+                            <li><Link to={'/'} className="navbar-brand"> 
                                 <img src={Logo} width="30" height="30" alt="" />
                             </Link>
                             </li>
